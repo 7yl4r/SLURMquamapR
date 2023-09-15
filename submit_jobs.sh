4a60c03e578d7a3bc0b6078684a5375cbde32cd2
Tylar Murray [tylarmurray] <tylarmurray@itn0.rc.usf.edu>
2023-09-14 20:48:52 -0400

    get working with SLURM on CIRCE

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name ="mbon_aquamaps"
 #SBATCH --nodes=1
-#SBATCH --TIME=3:00:00
 #SBATCH --array=0-2
 ## Can submit up to 10,000 jobs at once, but only 512 will run concurrently
 ## ============================================================================
@@ -14,18 +13,12 @@
 # Python code to check processing time:
 #    starttime = datetime.today()
 #    LogMsg('Image: %s' %(info.srcfn))
+set -e
 
 # === Set up input arguments & file locations
 TYLAR_DIR=/work/t/tylarmurray
 CODE_REPO_DIR=$TYLAR_DIR/repos/SLURMquamapR
 DATA_DIR=$TYLAR_DIR/data  # TODO: set in + out data dirs
-
-
-# === testing vars only
-TYLAR_DIR=/home/tylar
-CODE_REPO_DIR=$TYLAR_DIR/repos/SLURMquamapR
-DATA_DIR=$TYLAR_DIR/repos/SLURMquamapR
-SLURM_ARRAY_TASK_ID=1
 
 # === get species name from file
 species_names=`cat $CODE_REPO_DIR/species_names.txt`
@@ -33,13 +26,12 @@ species_names_arr=($species_names)  # cast to bash array
 species_name=${species_names_arr[$SLURM_ARRAY_TASK_ID]}
 echo running for species \"$species_name\"
 
-
 OUTPUT_FILEPATH=$DATA_DIR/$species_name.tif
 
 ## === Run the code
 expected_output_filepath=$DATA_DIR/$species_name.filetype  # TODO
 if [ ! -f $OUTPUT_FILEPATH ]; then  # if output file DNE
-    apps/R/4.0.5
+    module load apps/R/4.0.5
     Rscript $CODE_REPO_DIR/test_script.R $species_name  # TODO: create this script
 fi
 
